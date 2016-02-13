53a49b957e00c3027c092d82832727cb85686b5b
wangchunyu <wangchunyu3@jd.com>
2016-02-13 15:37:57 +0800

    webpack config css loader

--- tutorials01/webpack.config.js
+++ tutorials01/webpack.config.js
@@ -2,5 +2,13 @@ module.exports = {
   entry: './entry.js', // 源文件
   output: {
     filename: 'bundle.js' // 输出文件
+  },
+  module: {
+    loaders: [
+      {
+        test:/\.css$/,
+        loader: "style!css"
+      }
+    ]
   }
 };
