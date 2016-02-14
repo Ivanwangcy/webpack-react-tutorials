2bfcf916adfa18a26ce9a230f78bb46dda1afc41
Ivan <wcy5211678@163.com>
2016-02-14 09:55:34 +0800

    webpack multiple entry

--- tutorials02/webpack.config.js
+++ tutorials02/webpack.config.js
@@ -1,9 +1,19 @@
 module.exports = {
-  entry: {
-    bundle1: './entry1.js',
-    bundle2: './entry2.js'
-  },
+//  多个文档，单独打包
+  // entry: {
+  //   bundle1: './entry1.js',
+  //   bundle2: './entry2.js'
+  // },
+  // output: {
+  //   filename: '[name].js'
+  // }
+
+  // 多个文件合并打包 
+  entry: [
+    './entry1.js',
+    './entry2.js'
+  ],
   output: {
-    filename: '[name].js'
+    filename: 'bundle.js'
   }
 };
