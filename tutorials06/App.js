084d004cbe9b6ead8ae90a74b008b417e400de60
Ivan <wcy5211678@163.com>
2016-04-17 21:09:09 +0800

    add tutorial06

---
+++ tutorials06/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <h1>Hello ES6, Hello React, Hello World</h1>
+    );
+  }
+}
---
+++ tutorials06/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Content from './module/Content';
+
+ReactDOM.render(
+  <div>
+  <App />
+  <Content />
+  </div>,
+  document.getElementById('root')
+)
