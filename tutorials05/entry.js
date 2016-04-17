22681938b8e892ccd5b3f9b17df79f006e70961b
Ivan <wcy5211678@163.com>
2016-04-17 17:49:48 +0800

    use bundle loader

--- tutorials05/entry.js
+++ tutorials05/entry.js
@@ -11,10 +11,22 @@ ReactDOM.render(
   document.querySelector(".header")
 );
 
-require.ensure(['./module/list'], function(require){
-  var ListView = require('./module/list').default;
-  ReactDOM.render(
-    <ListView />,
-    document.querySelector(".container")
-  );
+// require.ensure(['./module/list'], function(require){
+//   var ListView = require('./module/list').default;
+//   ReactDOM.render(
+//     <ListView />,
+//     document.querySelector(".container")
+//   );
+// })
+
+// 使用 bundle-loader 更简洁
+var load = require('bundle-loader!./module/list');
+
+load(function(file){
+    console.log(file);
+    var ListView = file.default;
+    ReactDOM.render(
+      <ListView />,
+      document.querySelector(".container")
+    );
 })
