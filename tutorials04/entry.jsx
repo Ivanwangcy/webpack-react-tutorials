1d5d2f0d7f2e8277ef90689d049ab6b004a4b461
Ivan <wcy5211678@163.com>
2016-04-17 11:07:35 +0800

    webpack react

--- tutorials04/entry.jsx
+++ tutorials04/entry.jsx
@@ -5,10 +5,10 @@ import './style.css';
 var Hello = React.createClass({
   render: function(){
     return (
-      <div className='content'>Hello dada! </div>
+      <div className='content'>Hello world! <i class="icon"></i></div>
     );
   }
-})
+})7
 
 ReactDOM.render(
   <Hello />,
