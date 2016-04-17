9c66c296b50e348caff7f5ab1fd0fd249403972a
Ivan <wcy5211678@163.com>
2016-04-17 12:07:42 +0800

    add uglifyjsplugin

--- tutorials04/webpack.config.js
+++ tutorials04/webpack.config.js
@@ -1,8 +1,19 @@
+var webpack = require('webpack');
+
+var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
 module.exports = {
   entry: './entry.jsx',
   output: {
     filename: 'bundle.js'
   },
+  plugins: [
+    new uglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ],
   module: {
     loaders: [
       {
