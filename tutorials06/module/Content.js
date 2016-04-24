4c7dbf5d0d1efda308568174ed1a158cd086663d
Ivan <wcy5211678@163.com>
2016-04-24 16:50:59 +0800

    content modified

--- tutorials06/module/Content.js
+++ tutorials06/module/Content.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Content extends Component {
   render() {
     return (
-      <div>Hello Content</div>
+      <div>Hello React!</div>
     );
   }
 }
