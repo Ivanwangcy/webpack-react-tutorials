035d8ca198d2d4eb28e0f39b4af5896dd49dcdf3
Ivan <wcy5211678@163.com>
2016-04-30 21:29:57 +0800

    add ejs template

--- tutorials07/build/bundle.js
+++ tutorials07/build/bundle.js
@@ -59,7 +59,7 @@
 
 	function generateText() {
 	  var element = document.createElement("h2");
-	  element.innerHTML = "标题二";
+	  element.innerHTML = "标题二, 这是一个htmlWebpackPlugin示例, 根据ejs模板生成html文件";
 	  return element;
 	}
 
