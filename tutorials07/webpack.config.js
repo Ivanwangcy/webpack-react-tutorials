4bb2a6fde89205bec62b1912d897fc333e031227
Ivan <wcy5211678@163.com>
2016-04-30 21:17:14 +0800

    html-plugins

---
+++ tutorials07/webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+// 定义了一些文件夹的路径
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'src');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+var htmlTemplatePath = path.resolve(ROOT_PATH, 'src', 'assets', 'templates', 'index-template.ejs');
+console.log(htmlTemplatePath);
+
+module.exports = {
+  entry: APP_PATH,
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+  // 添加生成HTML文件的插件
+  plugins: [
+    new htmlWebpackPlugin({
+      title: "Hello World webpack app",
+      filename: 'index.html',
+      // template: './src/template.ejs',
+      template: htmlTemplatePath,
+      inject: true
+    })
+  ]
+};
