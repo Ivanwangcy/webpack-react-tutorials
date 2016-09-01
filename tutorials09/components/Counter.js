20c8d3bad721836a59b304cf81001c4a758c4514
Ivan <wcy5211678@163.com>
2016-09-01 11:29:46 +0800

    counter redux

--- tutorials09/components/Counter.js
+++ tutorials09/components/Counter.js
@@ -7,9 +7,48 @@ class Counter extends Component {
 
   }
 
-  render(){
-
+  incrementIfOdd(){
+    if(this.props.value % 2 !== 0){
+      this.props.onIncrement();
+    }
   }
+
+  incrementSync(){
+    setTimeout(
+      this.props.onIncrement
+    , 1000);
+  }
+
+  render(){
+    const { value, onIncrement, onDecrement } = this.props;
+    return (
+      <p>
+        Clicked: { value } times
+        {' '}
+        <button onClick={onIncrement}>
+          +
+        </button>
+        {' '}
+        <button onClick={onDecrement}>
+          -
+        </button>
+        {' '}
+        <button onClick={this.incrementIfOdd}>
+          Increment if odd
+        </button>
+        {' '}
+        <button onClick={this.incrementAsync}>
+          Increment async
+        </button>
+      </p>
+    )
+  }
+}
+
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired
 }
 
 export default Counter;
