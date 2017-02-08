cf62ca55459eb4956e647d2d92fc1b5b3ab9571f
Ivan <wcy5211678@163.com>
2017-02-08 14:07:22 +0800

    tutorials

--- tutorials01/entry.js
+++ tutorials01/entry.js
@@ -1,3 +1,9 @@
 // document.write('Hello World!');
 require('./style.css');
 document.write(require("./content.js"));
+
+var item = "<div>"+ name + "<div>";
+var items = []
+  items.map(function(value){
+    return "<li>"+ name + "<li>";
+  })
